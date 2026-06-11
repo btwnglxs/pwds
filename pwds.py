8a85fc8fb4d4c7ea01eb50f90d5b4b892b137f52
btwnglxs <kishouyukimura@gmail.com>
2026-06-12 01:21:42 +0300

    fixed critical logical flaws

--- pwds.py
+++ pwds.py
@@ -15,239 +15,208 @@ RichHelpFormatter.text_markup = True
 RichHelpFormatter.help_markup = True
 
 parser = argparse.ArgumentParser(
-	description="[bold white]Python Web Directory Scanner[/] [bright_black]by[/] [bold blue]btwnglxs[/]",
-	formatter_class=RichHelpFormatter,
-	epilog="[bright_black]EXAMPLE : python dir_scanner.py --host 192.168.0.1 --wordlist wordlist.txt --depth 2[/]"
+    description="[bold white]Python Web Directory Scanner[/] [bright_black]by[/] [bold blue]btwnglxs[/]",
+    formatter_class=RichHelpFormatter,
+    epilog="[bright_black]EXAMPLE : python dir_scanner.py --host 192.168.0.1 --wordlist wordlist.txt --depth 2[/]"
 )
 
-parser.add_argument("--target", type=str, required=True, help="Host to scan [bright_black](for ex.: 192.168.0.1)[/]")
-parser.add_argument("--wordlist", type=str, required=True, help="Directories wordlist [bright_black](for ex.: wordlist.txt)")
-parser.add_argument("--threads", type=int, help="Scanning threads count [bright_black](default is 40, up to 80)[/]", default=40)
-parser.add_argument("--depth", type=int, help="Maximum recursion depth [bright_black](default is 4)[/]", default=4)
-parser.add_argument("--match_codes", type=str, help="List of match codes, separated by commas[bright_black](default is 200,301,302,401,403)[/]", default="200,301,302,401,403")
+parser.add_argument("--target",      type=str, required=True, help="Host to scan [bright_black](for ex.: 192.168.0.1)[/]")
+parser.add_argument("--wordlist",    type=str, required=True, help="Directories wordlist [bright_black](for ex.: wordlist.txt)")
+parser.add_argument("--threads",     type=int, help="Scanning threads count [bright_black](default is 40, up to 100)[/]", default=40)
+parser.add_argument("--depth",        type=int, help="Maximum recursion depth [bright_black](default is 4)[/]", default=4)
+parser.add_argument("--match_codes",  type=str, help="List of match codes, separated by commas[bright_black](default is 200,301,302,401,403)[/]", default="200,301,302,401,403")
 
 args = parser.parse_args()
 
 valid_codes = [int(code.strip()) for code in args.match_codes.split(",")]
 
 wordlist_queue = queue.Queue()
-host = f"http://{args.target}"
+host = f"http://{args.target}" if not args.target.startswith(("http://", "https://")) else args.target
 founded_directories = []
 
-state_lock = threading.Lock()
+scanned_lock  = threading.Lock()
+results_lock  = threading.Lock()
+progress_lock = threading.Lock()
+
 scanned_paths = set()
-active_workers = 0
+bad_size      = None
+wordlist      = []
 
-bad_size = None
-
-wordlist = []
-
-if args.threads > 100:
-	THREADS = 100
-else:
-	THREADS = args.threads
+THREADS = min(args.threads, 100)
 
 try:
-	with open(args.wordlist, "r") as f:
-		for line in f:
-			word = line.strip()
-			if word:
-				wordlist.append(word)
+    with open(args.wordlist, "r") as f:
+        for line in f:
+            word = line.strip()
+            if word and not word.startswith("#"):
+                wordlist.append(word)
 except FileNotFoundError:
-	print(f"[bold red][!][/] File '{args.wordlist}' is not found.")
-	sys.exit()
+    print(f"[bold red][!][/] File '{args.wordlist}' is not found.")
+    sys.exit()
 except PermissionError:
-	print(f"[bold red][!][/] Permission error. Can't read file '{args.wordlist}'")
-	sys.exit()
+    print(f"[bold red][!][/] Permission error. Can't read file '{args.wordlist}'")
+    sys.exit()
 
-for word in wordlist:
-	wordlist_queue.put((word, 0))
+wordlist_queue.put(("", 0))
+
 
 def banner():
-	print(r"""[bold blue]
+
+    print(r"""[bold blue]
                     _
    _ ____      ____| |___
-  | '_ \ \ /\ / / _` / __| [bold blue][bright_black]by[/] btwnglxs[/]
-  | |_) \ V  V / (_| \__ \ [bright_black]ver-0.1[/]
+  | '_ \ \ /\ / / _` / __| [bold blue][bright_black]by[/] [link=https://github.com]btwnglxs[/link][/]
+  | |_) \ V  V / (_| \__ \ [bright_black]ver-0.1.1[/]
   | .__/ \_/\_/ \__,_|___/ [bold white]Python Web Directory Scanner[/]
   |_|
 [/]""")
 
-	print("   --------------------------------------------------------------------")
-	print("  ")
-	print(f"   [bright_black]Target      [/]| [bold white]{host}[/]")
-	print(f"   [bright_black]Wordlist    [/]| [bold white]{args.wordlist}[/]")
-	print(f"   [bright_black]Threads     [/]| [bold white]{THREADS}[/]")
-	print(f"   [bright_black]Depth       [/]| [bold white]{args.depth}[/]")
-	print(f"   [bright_black]Match Codes [/]| [bold white]{args.match_codes}[/]")
-	print("  ")
-	print("   --------------------------------------------------------------------")
-	print("\n")
+    print("   [bright_black]--------------------------------------------------------------------[/]")
+    print("  ")
+    print(f"   [bold white]Target      [/]  [bright_black]{host}[/]")
+    print(f"   [bold white]Wordlist    [/]  [bright_black]{args.wordlist}[/]")
+    print(f"   [bold white]Threads     [/]  [bright_black]{THREADS} / 100[/]")
+    print(f"   [bold white]Depth       [/]  [bright_black]{args.depth} / ANY[/]")
+    print(f"   [bold white]Match Codes [/]  [bright_black]{args.match_codes}[/]")
+    print("  ")
+    print("   [bright_black]--------------------------------------------------------------------[/]")
+    print("\n")
+
 
 def get_bad_size():
 
-	global bad_size
+    global bad_size
 
-	try:
+    try:
+        with requests.get(f"{host}/1106451782365012783512783118644", timeout=(5, 5), allow_redirects=False, stream=True) as r:
+            bad_size = int(r.headers.get("content-length", 0))
 
-		r = requests.get(f"{host}/1106451782365012783512783118644", timeout=5, allow_redirects=False)
-		bad_size = int(r.headers.get('content-length', len(r.content)))
+    except requests.exceptions.RequestException:
+        bad_size = None
 
-	except requests.exceptions.RequestException:
-		bad_size = None
+    except Exception as e:
+        print(f"[bold red][!][/] Error: {e}")
+        sys.exit()
 
-	except Exception as e:
-		print(f"[bold red][!][/] Error: {e}")
 
 def scan_dir(progress, task_id):
-	global active_workers
 
-	while True:
-		try:
-			directory, current_depth = wordlist_queue.get(timeout=0.1)
-		except queue.Empty:
-			with state_lock:
-				if active_workers == 0:
-					break
-			continue
+    session = requests.Session()
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
 
-		url = f"{host}/{directory}"
+    while True:
+        task = wordlist_queue.get()
 
-		with state_lock:
-			if url in scanned_paths:
-				progress.advance(task_id, advance=1)
-				continue
-			scanned_paths.add(url)
-			active_workers += 1
+        if task is None:
+            wordlist_queue.task_done()
+            break
 
-		try:
-			headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+        base_directory, current_depth = task
 
-			r = requests.get(url, headers=headers, timeout=2, allow_redirects=False)
+        try:
+            for word in wordlist:
 
-			length = r.headers.get('content-length')
+                directory = (f"{base_directory}/{word}" if base_directory else word)
 
-			if r.status_code in valid_codes :
-				if bad_size is None or length != bad_size:
-					with state_lock:
-						founded_directories.append((directory, r.status_code))
+                with scanned_lock:
+                    if directory in scanned_paths:
+                        with progress_lock:
+                            progress.advance(task_id)
+                        continue
 
-					progress.print(f"   [bold green][+][/] [bold white]/{directory}[/] [bright_black](Status: {r.status_code}, Depth: {current_depth}, CL: {length})[/]")
+                    scanned_paths.add(directory)
 
-					if current_depth < args.depth:
-						with state_lock:
-							added_count = 0
-							for word in wordlist:
-								new_path = f"{directory}/{word}"
-								new_url = f"{host}/{new_path}"
-								if new_url not in scanned_paths:
-									wordlist_queue.put((new_path, current_depth + 1))
-									added_count += 1
+                url = f"{host}/{directory}"
 
-							if added_count > 0:
-								progress.update(task_id, total=progress.tasks[task_id].total + added_count)
+                try:
+                    with session.get(url, headers=headers, timeout=(2, 2), allow_redirects=False, stream=True) as r:
+                        length = int(r.headers.get("content-length", 0))
 
-		except requests.exceptions.RequestException:
-			pass
-		finally:
-			with state_lock:
-				active_workers -= 1
-			progress.advance(task_id, advance=1)
+                        if (r.status_code in valid_codes and (bad_size is None or length != bad_size)):
+                            with results_lock:
+                                founded_directories.append((directory, r.status_code))
 
-def build_nested_dict(paths):
-	root = {}
-	for path, code in paths:
-		parts = path.strip("/").split("/")
-		current_level = root
-		for i, part in enumerate(parts):
-			if part not in current_level:
-				current_level[part] = {}
+                            with progress_lock:
+                                progress.print(f"   [bold green][+][/] "f"[bold white]/{directory}[/] [bright_black]({r.status_code}, Depth: {current_depth}, CL: {length})[/]")
 
-			if i == len(parts) - 1:
-				current_level[part]["_code"] = code
+                            if (r.status_code in (301, 302) and current_depth < args.depth):
+                                wordlist_queue.put((directory, current_depth + 1))
 
-			current_level = current_level[part]
-	return root
+                                with progress_lock:
+                                    progress.update(task_id, total=(progress.tasks[task_id].total + len(wordlist)))
 
-def add_nodes_to_rich_tree(rich_node, nested_dict):
-	for key, value in sorted(nested_dict.items()):
-		if key == "_code":
-			continue
+                except requests.exceptions.RequestException:
+                    pass
 
-		code = value.get("_code", None)
-		if code == 200:
-			label = f"[bold green]{key}[/] [bright_black]({code})[/]"
-		elif code in [301, 302]:
-			label = f"[bold yellow]{key}[/] [bright_black]({code})[/]"
-		elif code in [401, 403]:
-			label = f"[bold red]{key}[/] [bright_black]({code})[/]"
-		else:
-			label = f"[white]{key}[/]"
+                finally:
+                    with progress_lock:
+                        progress.advance(task_id)
 
-		branch = rich_node.add(label, guide_style="bright_black")
+        finally:
+            wordlist_queue.task_done()
 
-		add_nodes_to_rich_tree(branch, value)
+    session.close()
 
 
 def main():
 
-	banner()
+    banner()
+    threads = []
+    get_bad_size()
 
-	threads = []
+    try:
+        with Progress() as progress:
+            task = progress.add_task("[bold white]   Scanning...[/]", total=len(wordlist))
 
-	get_bad_size()
+            for _ in range(THREADS):
+                t = threading.Thread(target=scan_dir, args=(progress, task), daemon=True)
+                threads.append(t)
+                t.start()
 
-	try:
-		with Progress() as progress:
-			task = progress.add_task(f"   [bold white]Scanning...[/] ", total=len(wordlist))
+            wordlist_queue.join()
 
-			for i in range(THREADS):
-				t = threading.Thread(target=scan_dir, args=(progress, task))
-				t.daemon = True
-				threads.append(t)
-				t.start()
+            for _ in range(THREADS):
+                wordlist_queue.put(None)
 
-			for t in threads:
-				t.join()
+            for t in threads:
+                t.join()
 
-		print(f"\n   Scanning '[bold cyan]{host}[/]' is [bold green]complete[/].\n")
+        print("\n   [bright_black]--------------------------------------------------------------------[/]")
 
-		if founded_directories:
+        print(f"\n   Scanning '[bold cyan]{host}[/]' is [bold green]complete[/].\n")
 
-			print(f"   [bold blue]📂 {host}[/]")
+        if founded_directories:
+            print(f"   [bold white]📂 {args.target}[/]")
+            sorted_directories = sorted(founded_directories, key=lambda x: x[0])
 
-			sorted_directories = sorted(founded_directories, key=lambda x: x[0])
+            for i, (path, code) in enumerate(sorted_directories):
+                depth   = path.count("/")
+                is_last = (i == len(sorted_directories) - 1)
+                icon    = "╰" if is_last else "├"
+                lines   = "─" * (2 + depth * 4)
+                name    = path.split("/")[-1]
 
-			for i, (path, code) in enumerate(sorted_directories):
-				depth = path.count("/")
+                if code == 200:
+                    label = f"[bold green]{name}[/] [bright_black]{code}[/]"
+                elif code in [301, 302]:
+                    label = f"[bold yellow]{name}[/] [bright_black]{code}[/]"
+                elif code in [401, 403]:
+                    label = f"[bold red]{name}[/] [bright_black]{code}[/]"
+                else:
+                    label = f"[white]{name}[/] [bright_black]{code}[/]"
 
-				is_last = (i == len(sorted_directories) - 1)
-				icon = "╰" if is_last else "├"
+                print(f"   {icon}{lines} {label}")
 
-				lines = "─" * (2 + depth * 4)
+        else:
+            print("[bold red]   [!][/] No directories founded.")
 
-				name = path.split("/")[-1]
+    except KeyboardInterrupt:
+        print("\n[bold yellow]   [!] Keyboard interrupt. Quitting...[/]")
+        sys.exit()
+    except Exception as e:
+        print(f"   [bold red][!][/] Error : {e}")
+        sys.exit()
 
-				if code == 200:
-					label = f"[bold green]{name}[/] [bright_black]({code})[/]"
-				elif code in [301, 302]:
-					label = f"[bold yellow]{name}[/] [bright_black]({code})[/]"
-				elif code in [401, 403]:
-					label = f"[bold red]{name}[/] [bright_black]({code})[/]"
-				else:
-					label = f"[white]{name}[/]"
-
-				print(f"   {icon}{lines} {label}")
-		else:
-			print("[bold red][!][/] No directories founded.")
-
-
-	except KeyboardInterrupt:
-		print("\n   [bold orange][!] Keyboard interrupt. Exiting...[/]")
-		sys.exit()
-	except Exception as e:
-		print(f"   [bold red][!][/] Error : {e}")
-		sys.exit()
 
 if __name__ == "__main__":
-	main()
+    main()
